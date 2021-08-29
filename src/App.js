2c1a3b99e9a75d08bcfe251f035d9f9d1ec1a6c8
Jibendu Paul <bbiswas.paul@gmail.com>
2021-08-29 17:40:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Fragment} from 'react';
+import Calculator from './Components/calculator';
+
+
+const App = ()=>{
+    return(
+        <Fragment>
+            <Calculator />
+        </Fragment>
+    );
+}
+
+export default App;
